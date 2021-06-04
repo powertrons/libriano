63b69c20d208d850b9b2d99d31223ee823e1b8de
Leonardo Belafonte Alcaras <leonardo_belafonte@hotmail.com>
2021-06-04 17:22:23 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function home() {
+    return <html  align="center" lang="pt-BR"><head><title>Libriano</title><meta charset="UTF-8"/><meta name="viewport" content="width=device-width, initial-scale=1.0"/></head><body width="100%" height="100%"><h1>Hello</h1><hr/><h3>:3</h3></body></html>;
+}
+
+export default home;
